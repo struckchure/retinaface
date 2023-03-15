72aedeaff7e4c7c5dd092011787358fb994ab670
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-03-15 01:48:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.5.5',
-    install_requires=["numpy>=1.14.0", "gdown>=3.10.1", "Pillow>=5.2.0", "opencv-python>=3.4.4", "tensorflow>=1.9.0"]
+    install_requires=["numpy>=1.14.0", "gdown>=3.10.1", "Pillow>=5.2.0", "opencv-python>=3.4.4", "tensorflow-gpu>=1.9.0"]
 )
